235380158ce240ee2534992b6cc14347e718c667
Leopold Huelst <leopold.huelst@gmail.com>
2021-11-12 15:03:32 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'lists#index'
   get 'lists', to: 'lists#index'
   get 'lists/new', to: 'lists#new'
   get 'lists/:id', to: 'lists#show', as: 'list'
@@ -8,9 +9,4 @@ Rails.application.routes.draw do
   get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: 'bookmarks_new'
   post 'lists/:id/bookmarks', to: 'bookmarks#create', as: 'list_bookmarks'
   delete 'bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark_delete'
-
-# GET "lists/42/bookmarks/new"
-# POST "lists/42/bookmarks"
-# A user can delete a bookmark from a list. How can we make a delete link again?
-# DELETE "bookmarks/25"
 end
